b757817999661d8b12cb85f34c134b88fba12c4b
phatdroid94 <phatdroid94@gmail.com>
2020-12-08 18:38:13 +0700

    ui app

--- app/src/main/java/com/example/random24112020/MainActivity.java
+++ app/src/main/java/com/example/random24112020/MainActivity.java
@@ -13,41 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//      built in function : Các phương thức sẽ được gọi trực tiếp
-//      mà không cần khởi tạo lớp đối tượng
 
-
-//      Xem tham số truyền vào : ctrl + P
-//      Chuyển đổi kiểu dữ liệu
-
-//      Sử dụng phương thức chuyển đổi kiểu dữ liệu
-//        int a = 10;
-//        String value = String.valueOf(a);
-//        Log.d("BBB",value);
-
-        // 1 : Hàm làm tròn (Math.round)
-//        float a = 1.5f;
-//        double b = 1.5;
-//        int result = Math.round(a);
-//        Log.d("BBB",String.valueOf(result));
-//        2 : Hàm làm tròn lên (Math.ceil)
-//        double b = 1.01;
-//        double result2 = Math.ceil(b);
-//        Log.d("BBB",String.valueOf(result2));
-//        3 : Hàm làm tròn xuống (Math.floor)
-//        double c = 0.99999;
-//        double result3 = Math.floor(c);
-//        Log.d("BBB",String.valueOf(result3));
-//        4 : Hàm random
-//        Random trong khoảng từ 0 - 5
-//        double result4 = Math.round(Math.random() * 5);
-//        Log.d("BBB",String.valueOf(result4));
-
-
-        // 5 -> 10
-        Random random = new Random();
-        int result5 = random.nextInt(10 - 5 + 1) + 5;
-        Log.d("BBB",String.valueOf(result5));
 
     }
 }
